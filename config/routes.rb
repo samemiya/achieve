57264a262af74990c9803f116c93bcd26f808bc3
amemiya <samemiya@nttpc.co.jp>
2017-08-06 21:47:15 +0900

    170806201

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+  
+  # DIVE14_SNSログイン で編集
+  # OAuth機能を使えるようにする
+  devise_for :users, controllers: {
+    omniauth_callbacks: "users/omniauth_callbacks"
+  }
   # get 'top/index'
   
   # get 'blogs' => 'blogs#index' 
