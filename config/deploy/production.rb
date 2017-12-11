019694b5b86f54dfd5b44bac7e3a9343fb2a0eba
amemiya <samemiya@nttpc.co.jp>
2017-12-11 12:41:23 +0900

    init unicorn_1

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,9 +3,9 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
-server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
-server 'db.example.com', user: 'deploy', roles: %w{db}
+# server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
+# server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
+# server 'db.example.com', user: 'deploy', roles: %w{db}
 
 
 
@@ -17,9 +17,9 @@ server 'db.example.com', user: 'deploy', roles: %w{db}
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
 
-role :app, %w{deploy@example.com}, my_property: :my_value
-role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
-role :db,  %w{deploy@example.com}
+# role :app, %w{deploy@example.com}, my_property: :my_value
+# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
+# role :db,  %w{deploy@example.com}
 
 
 
@@ -38,15 +38,15 @@ role :db,  %w{deploy@example.com}
 # You may pass any option but keep in mind that net/ssh understands a
 # limited set of options, consult the Net::SSH documentation.
 # http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
-#
+
 # Global options
 # --------------
-set :ssh_options, {
-  keys: %w(/home/rlisowski/.ssh/id_rsa),
-  forward_agent: false,
-  auth_methods: %w(password)
-}
-#
+# set :ssh_options, {
+#   keys: %w(/home/rlisowski/.ssh/id_rsa),
+#   forward_agent: false,
+#   auth_methods: %w(password)
+# }
+
 # The server-based syntax can be used to override options:
 # ------------------------------------
 # server 'example.com',
@@ -64,5 +64,3 @@ set :ssh_options, {
 # デプロイ先のサーバ情報・アクセスキー情報を設定
 server '13.113.180.154', user: 'app', roles: %w{app db web}
 set :ssh_options, keys: '/c/Users/ame/.ssh/id_rsa'
-
-
